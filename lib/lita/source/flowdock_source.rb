006fb496071d99eaa0240f6c1e132f36239efaf2
Adam Panzer <apanzerj@gmail.com>
2015-05-12 05:40:33 -0700

    remove this bugger

--- lib/lita/source/flowdock_source.rb
+++ lib/lita/source/flowdock_source.rb
@@ -3,7 +3,6 @@ module Lita
     attr_reader :message_id
 
     def initialize(user: nil, room: nil, private_message: false, message_id: nil)
-      room = Lita.redis.get("flows/#{room}") unless room.nil?
       super(user: user, room: room, private_message: private_message)
       @message_id = message_id
     end
