93ef4f004860c0d1f0278ba3426499c29fa5b446
Adam Panzer <apanzerj@gmail.com>
2015-06-04 17:01:36 -0700

    test for @message_id being set

--- spec/lita/source/flowdock_source_spec.rb
+++ spec/lita/source/flowdock_source_spec.rb
@@ -10,4 +10,13 @@ describe Lita::FlowdockSource do
     expect(Lita.redis).to receive(:get).with("flows/#{room}").and_return(room_id)
     subject
   end
+
+  describe "with a message id" do
+    subject{ Lita::FlowdockSource.new(room: room, message_id: 123) }
+
+    it "saves the message id" do
+      allow(Lita.redis).to receive(:get).with("flows/#{room}").and_return(room_id)
+      expect(subject.message_id).to eql(123)
+    end
+  end
 end
