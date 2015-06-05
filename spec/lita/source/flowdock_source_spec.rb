aa6cf2a343a79b97d6dc3a796e1fc6595c02c470
Adam Panzer <apanzerj@gmail.com>
2015-06-04 17:04:28 -0700

    Test no room

--- spec/lita/source/flowdock_source_spec.rb
+++ spec/lita/source/flowdock_source_spec.rb
@@ -6,10 +6,22 @@ describe Lita::FlowdockSource do
 
   subject{ Lita::FlowdockSource.new(room: room) }
 
-  it "looks up rooms" do
-    expect(Lita.redis).to receive(:get).with("flows/#{room}").and_return(room_id)
-    subject
+  describe "with a room" do
+    it "looks up rooms" do
+      expect(Lita.redis).to receive(:get).with("flows/#{room}").and_return(room_id)
+      subject
+    end
   end
+
+  describe "without a room" do
+    subject{ Lita::FlowdockSource.new(user: "Bob") }
+
+    it "skips the room lookup if no room is given" do
+      expect(Lita.redis).not_to receive(:get).with("flows/#{room}")
+      subject
+    end
+  end
+
 
   describe "with a message id" do
     subject{ Lita::FlowdockSource.new(room: room, message_id: 123) }
